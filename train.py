60446bf4263bf3abdfda7e67a90af5dc7beb657a
username <email>
2021-06-08 17:05:38 -0400

    batch_size = v.size(0)  # v: [128, 36, 2048]

--- train.py
+++ train.py
@@ -22,7 +22,7 @@ from model.position_emb import prepare_graph_variables
 def instance_bce_with_logits(logits, labels, reduction='mean'):
     assert logits.dim() == 2
     loss = F.binary_cross_entropy_with_logits(
-                                logits, labels, reduction=reduction)
+        logits, labels, reduction=reduction)
     if reduction == "mean":
         loss *= labels.size(1)
     return loss
@@ -47,10 +47,10 @@ def train(model, train_loader, eval_loader, args, device=torch.device("cuda")):
     gradual_warmup_steps = [0.5 * lr_default, 1.0 * lr_default,
                             1.5 * lr_default, 2.0 * lr_default]
 
-    optim = torch.optim.Adamax(filter(lambda p: p.requires_grad,
-                                      model.parameters()),
-                               lr=lr_default, betas=(0.9, 0.999), eps=1e-8,
-                               weight_decay=args.weight_decay) 
+    optimizer = torch.optim.Adamax(filter(lambda p: p.requires_grad,
+                                          model.parameters()),
+                                   lr=lr_default, betas=(0.9, 0.999), eps=1e-8,
+                                   weight_decay=args.weight_decay)
 
     logger = utils.Logger(os.path.join(args.output, 'log.txt'))
     # best_eval_score = 0
@@ -59,8 +59,8 @@ def train(model, train_loader, eval_loader, args, device=torch.device("cuda")):
     logger.write('optim: adamax lr=%.4f, decay_step=%d, decay_rate=%.2f,'
                  % (lr_default, args.lr_decay_step,
                     args.lr_decay_rate) + 'grad_clip=%.2f' % args.grad_clip)
-    logger.write('LR decay epochs: '+','.join(
-                                        [str(i) for i in lr_decay_epochs]))
+    logger.write('LR decay epochs: ' + ','.join(
+        [str(i) for i in lr_decay_epochs]))
     last_eval_score, eval_score = 0, 0
     relation_type = train_loader.dataset.relation_type
 
@@ -71,29 +71,28 @@ def train(model, train_loader, eval_loader, args, device=torch.device("cuda")):
         count, average_loss, att_entropy = 0, 0, 0
         t = time.time()
         if epoch < len(gradual_warmup_steps):
-            for i in range(len(optim.param_groups)):
-                optim.param_groups[i]['lr'] = gradual_warmup_steps[epoch]
+            for i in range(len(optimizer.param_groups)):
+                optimizer.param_groups[i]['lr'] = gradual_warmup_steps[epoch]
             logger.write('gradual warmup lr: %.4f' %
-                         optim.param_groups[-1]['lr'])
+                         optimizer.param_groups[-1]['lr'])
         elif (epoch in lr_decay_epochs or
               eval_score < last_eval_score and args.lr_decay_based_on_val):
-            for i in range(len(optim.param_groups)):
-                optim.param_groups[i]['lr'] *= args.lr_decay_rate
-            logger.write('decreased lr: %.4f' % optim.param_groups[-1]['lr'])
+            for i in range(len(optimizer.param_groups)):
+                optimizer.param_groups[i]['lr'] *= args.lr_decay_rate
+            logger.write('decreased lr: %.4f' % optimizer.param_groups[-1]['lr'])
         else:
-            logger.write('lr: %.4f' % optim.param_groups[-1]['lr'])
+            logger.write('lr: %.4f' % optimizer.param_groups[-1]['lr'])
         last_eval_score = eval_score
 
         mini_batch_count = 0
         batch_multiplier = args.grad_accu_steps
         for i, (v, norm_bb, q, target, _, _, bb, spa_adj_matrix,
                 sem_adj_matrix) in enumerate(train_loader):
-            print(v.size())
-            batch_size = v.size(0)
+            batch_size = v.size(0)  # v: [128, 36, 2048]
             num_objects = v.size(1)
             if mini_batch_count == 0:
-                optim.step()
-                optim.zero_grad()
+                optimizer.step()
+                optimizer.zero_grad()
                 mini_batch_count = batch_multiplier
 
             v = Variable(v).to(device)
@@ -129,9 +128,9 @@ def train(model, train_loader, eval_loader, args, device=torch.device("cuda")):
                     att_entropy /= count
                     average_loss /= count
                     print("step {} / {} (epoch {}), ave_loss {:.3f},".format(
-                            i, len(train_loader), epoch,
-                            average_loss),
-                          "att_entropy {:.3f}".format(att_entropy))
+                        i, len(train_loader), epoch,
+                        average_loss),
+                        "att_entropy {:.3f}".format(att_entropy))
                     average_loss = 0
                     count = 0
                     att_entropy = 0
@@ -142,7 +141,7 @@ def train(model, train_loader, eval_loader, args, device=torch.device("cuda")):
             eval_score, bound, entropy = evaluate(
                 model, eval_loader, device, args)
 
-        logger.write('epoch %d, time: %.2f' % (epoch, time.time()-t))
+        logger.write('epoch %d, time: %.2f' % (epoch, time.time() - t))
         logger.write('\ttrain_loss: %.2f, norm: %.4f, score: %.2f'
                      % (total_loss, total_norm / count_norm, train_score))
         if eval_loader is not None:
@@ -154,11 +153,11 @@ def train(model, train_loader, eval_loader, args, device=torch.device("cuda")):
                 for i in range(entropy.size(0)):
                     info = info + ' %.2f' % entropy[i]
                 logger.write('\tentropy: ' + info)
-        if (eval_loader is not None)\
-           or (eval_loader is None and epoch >= args.saving_epoch):
+        if (eval_loader is not None) \
+                or (eval_loader is None and epoch >= args.saving_epoch):
             logger.write("saving current model weights to folder")
             model_path = os.path.join(args.output, 'model_%d.pth' % epoch)
-            opt = optim if args.save_optim else None
+            opt = optimizer if args.save_optim else None
             utils.save_model(model_path, model, epoch, opt)
 
 
@@ -191,11 +190,11 @@ def evaluate(model, dataloader, device, args):
         pred, att = model(v, norm_bb, q, pos_emb, sem_adj_matrix,
                           spa_adj_matrix, target)
         batch_score = compute_score_with_logits(
-                        pred, target, device).sum()
+            pred, target, device).sum()
         score += batch_score
         upper_bound += (target.max(1)[0]).sum()
         num_data += pred.size(0)
-        if att is not None and 0 < model.module.glimpse\
+        if att is not None and 0 < model.module.glimpse \
                 and entropy is not None:
             entropy += calc_entropy(att.data)[:model.module.glimpse]
         pbar.update(1)
